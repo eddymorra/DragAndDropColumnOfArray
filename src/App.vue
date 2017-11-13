3aaf2168bac4ca63bb3030aa6ab7096784ca7934
eddymorra <thibaut.cout@gmail.com>
2017-11-13 15:06:06 +0100

    Evolution et perfectionnement

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
-  <div id="app"> 
-    <hello></hello>
-  </div>
+    <div>
+        <div class="topbar">
+            <h1>Gestion de tableau</h1>
+        </div>
+
+        <div class="menugauche">
+            <ul>
+                <li>Menu</li>
+                <li>Accueil</li>
+                <li>Item menu 2</li>
+                <li>Item menu 3</li>
+                <li>Item menu 4</li>
+                <li>Item menu 5</li>
+                <li>Item menu 6</li>
+                <li>Item menu 7</li>
+                <li>Item menu 8</li>
+                <li>Item menu 9</li>
+                <li>Dernier Item du menu</li>
+            </ul>
+        </div>
+        <div id="app"> 
+            <tab></tab>
+        </div>
+    </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+    import Tab from './components/Tab';
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+    export default {
+    name: 'app',
+    components: {
+        'tab': Tab
+    }
+    }
 </script>
 
 <style>
@@ -24,74 +45,78 @@ export default {
 }
 
 html {
-    background-color: #eeeeee;
+    background-color: #ffffff;
     width: 100%; height: 100%;
 }
 
 .topbar {
     position: fixed;
-    width: 100%; height: 50px;
-    background-color: #678;
-    border-bottom: 1px solid black;
+    width: 100%; height: 42px;
+    background-color: #222222;
+    border-bottom: 6px solid #40E583;
 }
 
     .topbar h1 {
-        margin: 8px 0 0 10px;
+        margin: 7px 0 0 10px;
+        font-size: 24px;
         color: #eeeeee;
     }
 
 .menugauche {
     position: fixed;
     float: left;
-    margin-top: 50px;
-    width: 20%; height: calc(100% - 50px);
-    border-right: 1px solid black;
-    padding-left: 40px;
-    overflow-y: scroll;
-    background-color: #ffffff;
+    margin: 70px 0 0 20px;
+    width: 15%;
+    padding: 6px 0 0 0;
+    background-color: #111111;
+    border-radius: 4px;
+        -moz-box-shadow: 0px 0px 7px 0px #656565;
+        -webkit-box-shadow: 0px 0px 7px 0px #656565;
+        -o-box-shadow: 0px 0px 7px 0px #656565;
+        box-shadow: 0px 0px 7px 0px #656565;
+        filter:progid:DXImageTransform.Microsoft.Shadow(color=#656565, Direction=NaN, Strength=7);
 }
 
     .menugauche > ul > li {
-        font-size: 20px;
-        color: #999999;
-        line-height: 40px;
+        padding-left: 20px;
     }
 
-.corps {
-    position: fixed;
-    background-color: #bcd;
-    top: 90px;
-    right: 40px;
-    width: calc(80% - 80px); height: calc(100% - 150px);
-    border: 1px solid black;
-    padding: 10px;
-    overflow-y: scroll;
+    .menugauche > ul > li:nth-child(n+2) {
+        font-size: 16px;
+        color: #40E583;
+        line-height: 30px;
+        cursor: pointer;
+        background-color: #333333;
+        list-style: none;
+        
+    }
+
+    .menugauche > ul > li:first-child {
+        background-color: #111111;
+        font-size: 18px;
+        color: #ffffff;
+        line-height: 34px;
+        border-bottom: 4px solid #40E583;
+    }
+
+    .menugauche > ul > li:hover:nth-child(n+2) {
+        color: #333333;
+        background-color: #40E583;
+    }
+
+::-webkit-scrollbar-track
+{
+	background-color: #444444;
 }
 
-.incorps {
-  width: 100%; height: 100%;
-  position: relative;
+::-webkit-scrollbar
+{
+	width: 6px;
+	background-color: #444444;
 }
 
-  .corps .btnbtot1 {
-      background-color: #73a8ee;
-      margin: 10px auto;
-      text-align: center;
-      cursor: pointer;
-      color: blue;
-      font-size: 14px;
-      padding: 7px;
-  }
-
-.btnbtot2 {
-    display: none;
-    position: absolute;
-    bottom: 10px; right: 40px;
-    background-color: #73df8b;
-    text-align: center;
-    cursor: pointer;
-    color: blue;
-    font-size: 14px;
-    padding: 7px 18px;
+::-webkit-scrollbar-thumb
+{
+	background-color: #40E583;
 }
 </style>
